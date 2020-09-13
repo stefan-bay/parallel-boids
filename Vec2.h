d8f0ac018265a0c10bc0c205eea2ff4c308b79a2
stefan-bay <stefanbay1@gmail.com>
2020-09-13 14:41:43 -0600

    Fix naming (hopefully)

---
+++ Vec2.h
@@ -0,0 +1,51 @@
+#ifndef VEC2_H_
+#define VEC2_H_
+
+#include <vector>
+
+class Vec2 {
+
+public:
+    float x;
+    float y;
+
+    Vec2() {
+
+    }
+
+    Vec2(float x_, float y_) {
+        x = x_;
+        y = y_;
+    }
+
+    void set(float x, float y);
+
+    void addV(Vec2 v);
+    void addS(float scalar);
+
+    void subV(Vec2 v);
+    void subS(float x);
+
+    static Vec2 subtract(Vec2 v1, Vec2 v2);
+
+    void multiplyV(Vec2 v);
+    void multiplyS(float x);
+
+    void divideV(Vec2 v);
+    void divideS(float x);
+
+    float distance(Vec2 v) const;
+    float dot(Vec2 v) const;
+    float magnitude() const;
+    void setMagnitude(float x);
+    float angle(Vec2 v);
+    void normalize();
+
+    void limit(float m);
+
+    static Vec2 copy(Vec2 v);
+
+
+};
+
+#endif
